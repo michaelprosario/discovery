6bd7db654f7c177807c8e7cfee5543b0bf32feaf
Michael Rosario <michael.p.rosario@gmail.com>
2025-11-15 16:45:27 +0000

    fix: Remove 'br' from Accept-Encoding header to avoid extra dependency

--- src/infrastructure/providers/newspaper_web_fetch_provider.py
+++ src/infrastructure/providers/newspaper_web_fetch_provider.py
@@ -49,7 +49,7 @@ class Newspaper3kWebFetchProvider(IWebFetchProvider):
             "User-Agent": self.user_agent,
             "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
             "Accept-Language": "en-US,en;q=0.9",
-            "Accept-Encoding": "gzip, deflate, br",
+            "Accept-Encoding": "gzip, deflate",  # Removed 'br' - brotli not supported without extra dependency
             "DNT": "1",  # Do Not Track
             "Connection": "keep-alive",
             "Upgrade-Insecure-Requests": "1",
