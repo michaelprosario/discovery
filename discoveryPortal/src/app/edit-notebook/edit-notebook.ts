1bb4a1f40d99893b343bafcb90f53c40e19ddea7
Michael Rosario <michael.p.rosario@gmail.com>
2025-11-22 15:53:58 +0000

    feat (edit-notebook) enhance component with data loading and display functionality

--- discoveryPortal/src/app/edit-notebook/edit-notebook.ts
+++ discoveryPortal/src/app/edit-notebook/edit-notebook.ts
@@ -1,11 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, ChangeDetectorRef } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute, RouterModule } from '@angular/router';
+import { forkJoin } from 'rxjs';
+import { NotebookApiService } from '../infrastructure/http/notebook-api.service';
+import { SourceApiService } from '../infrastructure/http/source-api.service';
+import { NotebookResponse, SourceResponse } from '../core/models';
 
 @Component({
   selector: 'app-edit-notebook',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterModule],
   templateUrl: './edit-notebook.html',
   styleUrl: './edit-notebook.scss',
 })
-export class EditNotebook {
+export class EditNotebook implements OnInit {
+  private route = inject(ActivatedRoute);
+  private notebookService = inject(NotebookApiService);
+  private sourceService = inject(SourceApiService);
+  private cdr = inject(ChangeDetectorRef);
 
+  notebook: NotebookResponse | null = null;
+  sources: SourceResponse[] = [];
+  notebookId: string | null = null;
+  isLoading = true;
+
+  ngOnInit() {
+    this.notebookId = this.route.snapshot.paramMap.get('id');
+    if (this.notebookId) {
+      this.loadData(this.notebookId);
+    }
+  }
+
+  loadData(id: string) {
+    this.isLoading = true;
+    forkJoin({
+      notebook: this.notebookService.getNotebook(id),
+      sources: this.sourceService.listSourcesByNotebook({ notebook_id: id })
+    }).subscribe({
+      next: (results) => {
+        this.notebook = results.notebook;
+        this.sources = results.sources.sources;
+        this.isLoading = false;
+        this.cdr.detectChanges();
+      },
+      error: (err) => {
+        console.error('Error loading data', err);
+        this.isLoading = false;
+        this.cdr.detectChanges();
+      }
+    });
+  }
 }
